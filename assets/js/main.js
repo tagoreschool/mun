4428371db856a119440d92570fb389ee975314c6
Gaurav Pandey <yednapg@gmail.com>
2022-10-18 06:34:06 +0530

    fix date

--- assets/js/main.js
+++ assets/js/main.js
@@ -225,7 +225,7 @@
 })()
 
 // Set the date we're counting down to
-var countDownDate = new Date("Dec 4, 2022 09:00:00").getTime();
+var countDownDate = new Date("Dec 3, 2022 09:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
